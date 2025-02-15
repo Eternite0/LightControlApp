f59fdeea9b73ca6b997f0c5a760f49fba3aa2a88
Eternite0 <k.pol2547@gmail.com>
2025-02-15 19:10:24 +0700

    slider add
    add 5 slider with value

--- lib/Screen/item.dart
+++ lib/Screen/item.dart
@@ -8,22 +8,125 @@ class Item extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<Item> {
-
+  double sliderWidth = 300;
+  double Val0 = 0;
   double Val1 = 0;
+  double Val2 = 0;
+  double Val3 = 0;
+  double Val4 = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Slider(value: Val1, onChanged: (value) {
-            setState(() {
-              Val1 = value;
-            });
-          })
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: sliderWidth,
+                child: Slider(
+                  value: Val0,
+                  min: 0,
+                  max: 100,
+                  onChanged: (value) {
+                    setState(() {
+                      Val0 = value;
+                    });
+                  },
+                ),
+              ),
+              sidelabel(Val0)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: sliderWidth,
+                child: Slider(
+                  value: Val1,
+                  min: 0,
+                  max: 100,
+                  onChanged: (value) {
+                    setState(() {
+                      Val1 = value;
+                    });
+                  },
+                ),
+              ),
+              sidelabel(Val1)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: sliderWidth,
+                child: Slider(
+                  value: Val2,
+                  min: 0,
+                  max: 100,
+                  onChanged: (value) {
+                    setState(() {
+                      Val2 = value;
+                    });
+                  },
+                ),
+              ),
+              sidelabel(Val2)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: sliderWidth,
+                child: Slider(
+                  value: Val3,
+                  min: 0,
+                  max: 100,
+                  onChanged: (value) {
+                    setState(() {
+                      Val3 = value;
+                    });
+                  },
+                ),
+              ),
+              sidelabel(Val3)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: sliderWidth,
+                child: Slider(
+                  value: Val4,
+                  min: 0,
+                  max: 100,
+                  onChanged: (value) {
+                    setState(() {
+                      Val4 = value;
+                    });
+                  },
+                ),
+              ),
+              sidelabel(Val4)
+            ],
+          ),
         ],
-      ),
+      )),
     );
   }
+
+  Widget sidelabel(double value) => Container(
+        width: 30,
+        child: Text(
+          value.round().toString(),
+          textAlign: TextAlign.center,
+        ),
+      );
 }
