3fd19935d65fc1c3a8907a3d3db4348319b3dffa
Eternite0 <k.pol2547@gmail.com>
2025-02-26 22:59:30 +0700

    Bluetooth
    -Add example Bluetooth for test
    -Change slider(n) value will send "HelloWorld(n)" n = 0,1,2,3,4

--- lib/Screen/item.dart
+++ lib/Screen/item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class Item extends StatefulWidget {
   const Item({super.key});
@@ -16,7 +17,44 @@ class _MyWidgetState extends State<Item> {
   double Val4 = 0;
   bool mode = true;
 
+  BluetoothDevice? _device;
+  BluetoothCharacteristic? _characteristic;
+
   @override
+  void initState() {
+    super.initState();
+    connectToFirstPairedDevice(); // Auto-connect on startup
+  }
+
+  void connectToFirstPairedDevice() async {
+    List<BluetoothDevice> bondedDevices = await FlutterBluePlus.bondedDevices;
+    if (bondedDevices.isNotEmpty) {
+      _device = bondedDevices.first; // Connect to the first paired device
+      await _device!.connect();
+      discoverServices();
+    }
+  }
+
+  void discoverServices() async {
+    if (_device == null) return;
+    List<BluetoothService> services = await _device!.discoverServices();
+    for (var service in services) {
+      for (var characteristic in service.characteristics) {
+        if (characteristic.properties.write) {
+          setState(() {
+            _characteristic = characteristic;
+          });
+        }
+      }
+    }
+  }
+
+  void sendData(String message) async {
+    if (_characteristic != null) {
+      await _characteristic!.write(message.codeUnits);
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -36,6 +74,7 @@ class _MyWidgetState extends State<Item> {
                     setState(() {
                       Val0 = value;
                       mode = false;
+                      sendData("HelloWorld0");
                     });
                   },
                 ),
@@ -56,6 +95,7 @@ class _MyWidgetState extends State<Item> {
                     setState(() {
                       Val1 = value;
                       mode = false;
+                      sendData("HelloWorld1");
                     });
                   },
                 ),
@@ -76,6 +116,7 @@ class _MyWidgetState extends State<Item> {
                     setState(() {
                       Val2 = value;
                       mode = false;
+                      sendData("HelloWorld2");
                     });
                   },
                 ),
@@ -96,6 +137,7 @@ class _MyWidgetState extends State<Item> {
                     setState(() {
                       Val3 = value;
                       mode = false;
+                      sendData("HelloWorld3");
                     });
                   },
                 ),
@@ -116,6 +158,7 @@ class _MyWidgetState extends State<Item> {
                     setState(() {
                       Val4 = value;
                       mode = false;
+                      sendData("HelloWorld4");
                     });
                   },
                 ),
