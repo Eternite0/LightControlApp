3dd9352bd62d5d769cf4642bc29bbeebecace49f
Eternite0 <k.pol2547@gmail.com>
2025-03-13 22:33:53 +0700

    Background
    -Decoration background with best image

--- lib/Screen/item.dart
+++ lib/Screen/item.dart
@@ -10,8 +10,8 @@ class Item extends StatefulWidget {
 
 class _MyWidgetState extends State<Item> {
   double sliderWidth = 300;
-  List<double> dataPercent = [0,0,0,0,0];
-  List<double> dataForCom = [0,0,0,0,0];
+  List<double> dataPercent = [0, 0, 0, 0, 0];
+  List<double> dataForCom = [0, 0, 0, 0, 0];
   bool mode = true;
 
   BluetoothDevice? _device;
@@ -55,131 +55,139 @@ class _MyWidgetState extends State<Item> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+          decoration: BoxDecoration(image: DecorationImage(image: AssetImage("images/rickroll.jpg"),fit: BoxFit.cover)),
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(dataForCom[0].toString()),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              SizedBox(
-                width: sliderWidth,
-                child: Slider(
-                  value: dataPercent[0],
-                  min: 0,
-                  max: 100,
-                  onChanged: (value) {
-                    setState(() {
-                      dataPercent[0] = (value > 99) ? 100 : value;
-                      dataForCom[0] = (value > 99) ? 1023 : (910*value)/99;
-                      mode = false;
-                      sendData("HelloWorld0");
-                    });
-                  },
-                ),
+              Text(dataForCom[0].toString()),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: sliderWidth,
+                    child: Slider(
+                      value: dataPercent[0],
+                      min: 0,
+                      max: 100,
+                      onChanged: (value) {
+                        setState(() {
+                          dataPercent[0] = (value > 99) ? 100 : value;
+                          dataForCom[0] =
+                              (value > 99) ? 1023 : (910 * value) / 99;
+                          mode = false;
+                          sendData("HelloWorld0");
+                        });
+                      },
+                    ),
+                  ),
+                  sidelabel(dataPercent[0])
+                ],
               ),
-              sidelabel(dataPercent[0])
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: sliderWidth,
-                child: Slider(
-                  value: dataPercent[1],
-                  min: 0,
-                  max: 100,
-                  onChanged: (value) {
-                    setState(() {
-                      dataPercent[1] = (value > 99) ? 100 : value;
-                      dataForCom[1] = (value > 99) ? 1023 : (910*value)/99;
-                      mode = false;
-                      sendData("HelloWorld1");
-                    });
-                  },
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: sliderWidth,
+                    child: Slider(
+                      value: dataPercent[1],
+                      min: 0,
+                      max: 100,
+                      onChanged: (value) {
+                        setState(() {
+                          dataPercent[1] = (value > 99) ? 100 : value;
+                          dataForCom[1] =
+                              (value > 99) ? 1023 : (910 * value) / 99;
+                          mode = false;
+                          sendData("HelloWorld1");
+                        });
+                      },
+                    ),
+                  ),
+                  sidelabel(dataPercent[1])
+                ],
               ),
-              sidelabel(dataPercent[1])
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: sliderWidth,
-                child: Slider(
-                  value: dataPercent[2],
-                  min: 0,
-                  max: 100,
-                  onChanged: (value) {
-                    setState(() {
-                      dataPercent[2] = (value > 99) ? 100 : value;
-                      dataForCom[2] = (value > 99) ? 1023 : (910*value)/99;
-                      mode = false;
-                      sendData("HelloWorld2");
-                    });
-                  },
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: sliderWidth,
+                    child: Slider(
+                      value: dataPercent[2],
+                      min: 0,
+                      max: 100,
+                      onChanged: (value) {
+                        setState(() {
+                          dataPercent[2] = (value > 99) ? 100 : value;
+                          dataForCom[2] =
+                              (value > 99) ? 1023 : (910 * value) / 99;
+                          mode = false;
+                          sendData("HelloWorld2");
+                        });
+                      },
+                    ),
+                  ),
+                  sidelabel(dataPercent[2])
+                ],
               ),
-              sidelabel(dataPercent[2])
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: sliderWidth,
-                child: Slider(
-                  value: dataPercent[3],
-                  min: 0,
-                  max: 100,
-                  onChanged: (value) {
-                    setState(() {
-                      dataPercent[3] = (value > 99) ? 100 : value;
-                      dataForCom[3] = (value > 99) ? 1023 : (910*value)/99;
-                      mode = false;
-                      sendData("HelloWorld3");
-                    });
-                  },
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: sliderWidth,
+                    child: Slider(
+                      value: dataPercent[3],
+                      min: 0,
+                      max: 100,
+                      onChanged: (value) {
+                        setState(() {
+                          dataPercent[3] = (value > 99) ? 100 : value;
+                          dataForCom[3] =
+                              (value > 99) ? 1023 : (910 * value) / 99;
+                          mode = false;
+                          sendData("HelloWorld3");
+                        });
+                      },
+                    ),
+                  ),
+                  sidelabel(dataPercent[3])
+                ],
               ),
-              sidelabel(dataPercent[3])
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: sliderWidth,
-                child: Slider(
-                  value: dataPercent[4],
-                  min: 0,
-                  max: 100,
-                  onChanged: (value) {
-                    setState(() {
-                      dataPercent[4] = (value > 99) ? 100 : value;
-                      dataForCom[4] = (value > 99) ? 1023 : (910*value)/99;
-                      mode = false;
-                      sendData("HelloWorld4");
-                    });
-                  },
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: sliderWidth,
+                    child: Slider(
+                      value: dataPercent[4],
+                      min: 0,
+                      max: 100,
+                      onChanged: (value) {
+                        setState(() {
+                          dataPercent[4] = (value > 99) ? 100 : value;
+                          dataForCom[4] =
+                              (value > 99) ? 1023 : (910 * value) / 99;
+                          mode = false;
+                          sendData("HelloWorld4");
+                        });
+                      },
+                    ),
+                  ),
+                  sidelabel(dataPercent[4])
+                ],
               ),
-              sidelabel(dataPercent[4])
+              OutlinedButton(
+                onPressed: () {
+                  setState(() {
+                    mode = !mode;
+                  });
+                },
+                child: mode ? Text("Auto") : Text("Maual"),
+                style: ButtonStyle(
+                    foregroundColor: WidgetStatePropertyAll<Color>(
+                        mode ? Colors.green : Colors.red)),
+              ),
             ],
-          ),
-          OutlinedButton(
-            onPressed: () {
-              setState(() {
-                mode = !mode;
-              });
-            },
-            child: mode ? Text("Auto") : Text("Maual"),
-            style: ButtonStyle(foregroundColor: WidgetStatePropertyAll<Color>(mode ? Colors.green : Colors.red)),
-          ),
-        ],
-      )),
+          )),
     );
   }
 
